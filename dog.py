4d6cf399fad4d969cac39395d66b610b339f3e6d
SvetaLanaTver <104692234+SvetaLanaTver@users.noreply.github.com>
2022-10-26 22:49:13 +0300

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self, imya, pol, vozrast):
+        self.imya = imya
+        self.pol = pol
+        self.vozrast = vozrast
+
+
---
+++ testDog.py
@@ -0,0 +1,10 @@
+from dog import Dog
+
+dog1 = Dog("Феликс", "мальчик", "2 года")
+dog2 = Dog("Мухтар", "мальчик", "0 лет")
+
+print("Собаки на сайте Дом питомца:")
+print("Кличка:", dog1.imya, ", пол:", dog1.pol, ", возраст:", dog1.vozrast)
+print("Кличка:", dog2.imya, ", пол:", dog2.pol, ", возраст:", dog2.vozrast)
+
+
